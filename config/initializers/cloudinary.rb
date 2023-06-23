fb643c90e96b4fa13082ce67f927f74486d1553a
Marnie Morstadt <marnielumo@gmail.com>
2023-06-23 14:18:01 +0100

    cloudinary

--- config/initializers/cloudinary.rb
+++
@@ -1,7 +0,0 @@
-Cloudinary.config do |config|
-  config.cloud_name = ENV['CLOUDINARY_URL'].match(/cloudinary:\/\/(\w+):(\w+)@(\w+)/)[3]
-  config.api_key = ENV['CLOUDINARY_URL'].match(/cloudinary:\/\/(\w+):(\w+)@(\w+)/)[1]
-  config.api_secret = ENV['CLOUDINARY_URL'].match(/cloudinary:\/\/(\w+):(\w+)@(\w+)/)[2]
-  config.secure = true
-  config.cdn_subdomain = true
-end
