777f38a68229ce75410a9d17f93ed012b233a23e
Marnie Morstadt <marnielumo@gmail.com>
2023-06-30 18:31:22 +0100

    changed the bubble mugs index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -43,7 +43,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    params.require(:product).permit(:name, :description, :price, :size,  photos: [])
+    params.require(:product).permit(:name, :description, :price, :size, photos: [])
   end
 
   def require_authorized_user
